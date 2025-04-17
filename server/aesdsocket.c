57aaed71e502affe83dbf726771893aa7f011a26
gordongu <1559005+gordongu@users.noreply.github.com>
2025-04-17 05:34:11 -0600

    Assignment 6 part 1 initial commit

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -5,42 +5,217 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/queue.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <syslog.h>
+#include <pthread.h>
 
 // Constants
 #define PORT "9000"
 #define BUFF_SIZE 1024
 const char *filepath = "/var/tmp/aesdsocketdata";
 
-// Dynamically allocated variables that need to be freed
-char *receive_buffer;
-char *write_buffer;
+// Timestamp handler thread ID
+pthread_t timestamp_thread_id;
+
+// Linked list declarations
+typedef struct thread_data{
+    pthread_t thread_id;
+    int socket_fd;
+    int completed;
+    LIST_ENTRY(thread_data) entries;
+} thread_data_t;
+
+LIST_HEAD(thread_data_list, thread_data) list_head;
+
+// Mutex declarations
+pthread_mutex_t file_access = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t thread_access = PTHREAD_MUTEX_INITIALIZER;
+
+volatile sig_atomic_t run = 1;
+
 struct addrinfo *servinfo;
-int socket_fd, new_socket_fd;
+int socket_fd;
 
 // Close sockets, free dynamically allocated memory, and delete file on interruption or termination signals
-void interrupt_signal_handler() {
-    close(socket_fd);
-    close(new_socket_fd);
-    free(receive_buffer);
-    free(write_buffer);
-    freeaddrinfo(servinfo);
-    if (remove(filepath) != 0) {
-        syslog(LOG_ERR, "Error: unable to delete temporary file");
-    }
+void signal_handler(int signo) {
     syslog(LOG_INFO, "Caught signal, exiting");
-    closelog();
-    exit(EXIT_SUCCESS);
+
+    if (signo == SIGINT || signo == SIGTERM) {
+        pthread_cancel(timestamp_thread_id);
+        pthread_join(timestamp_thread_id, NULL);
+
+        pthread_mutex_lock(&thread_access);
+        thread_data_t *thread = LIST_FIRST(&list_head);
+
+        while (thread != NULL) {
+            thread_data_t *next = LIST_NEXT(thread, entries);
+            pthread_cancel(thread->thread_id);
+            pthread_mutex_unlock(&thread_access);
+            pthread_join(thread->thread_id, NULL);
+            LIST_REMOVE(thread, entries);
+            pthread_mutex_lock(&thread_access);
+            free(thread);
+            thread = next;
+        }
+        pthread_mutex_unlock(&thread_access);
+
+        pthread_mutex_destroy(&file_access);
+        pthread_mutex_destroy(&thread_access);
+
+        freeaddrinfo(servinfo);
+
+        if (close(socket_fd) != 0) {
+            syslog(LOG_ERR, "Error: unable to close socket connection");
+        }
+
+        if (remove(filepath) != 0) {
+            syslog(LOG_ERR, "Error: unable to delete temporary file");
+        }
+
+        closelog();
+    }
+
+    run = 0;
+}
+
+void *timestamp_handler(void *arg) {
+    while (run) {
+        // Wait 10 seconds
+        sleep(10);
+
+        // Exit loop if termination occurred during sleep
+        if (run != 1) break;
+
+        char timestamp[BUFF_SIZE];
+        time_t current_time = time(NULL);
+        struct tm *tm_struct = localtime(&current_time);
+
+        // Create timestamp string in RFC 2822 compliant strftime format
+        strftime(timestamp, sizeof(timestamp), "timestamp:%a, %d %b %Y %H:%M:%S %z\n", tm_struct);
+
+        // Open and write timestamp to file
+        pthread_mutex_lock(&file_access);
+        FILE *fileptr = fopen(filepath, "a+");
+
+        if (fprintf(fileptr, "%s", timestamp) < 0) {
+            syslog(LOG_ERR, "Error: unable to write to file");
+            closelog();
+            exit(EXIT_FAILURE);
+        }
+
+        fclose(fileptr);
+        pthread_mutex_unlock(&file_access);
+    }
+}
+
+void *connection_handler(void *arg) {
+    // Get and cast thread params from arg
+    thread_data_t *thread = (thread_data_t *)arg;
+    int socket_fd = thread->socket_fd;
+
+    // Create and dynamically allocate memory to buffer
+    char *buffer = malloc(BUFF_SIZE);
+
+    ssize_t received;
+    int size = 0;
+
+    // Begin receiving data from incoming connection
+    while ((received = recv(socket_fd, buffer, BUFF_SIZE - 1, 0)) > 0) {
+        // Null terminate the end of the buffer
+        buffer[received] = '\0';
+
+        // Dynamically allocate more memory to buffer if received data exceeds default buffer size
+        if (size + received >= BUFF_SIZE) {
+            ssize_t new_buff_size = size + received + 1;
+
+            char *new_buffer = realloc(buffer, new_buff_size);
+
+            if (new_buffer == NULL) {
+                syslog(LOG_ERR, "Error: unable to reallocate memory for receiving buffer");
+                closelog();
+                exit(EXIT_FAILURE);
+            }
+
+            buffer = new_buffer;
+        }
+
+        // Update size and null terminate the end of the buffer
+        size += received;
+        buffer[size] = '\0';
+
+        // Check for newline in buffer
+        char *newline = memchr(buffer, '\n', size);
+        
+        // If newline is found, open file and begin writing to file from buffer
+        if (newline != NULL) {
+            pthread_mutex_lock(&file_access);
+            FILE *fileptr = fopen(filepath, "a+");
+
+            // Write to file from buffer
+            if (fprintf(fileptr, "%s", buffer) < 0) {
+                syslog(LOG_ERR, "Error: unable to write to file");
+                closelog();
+                exit(EXIT_FAILURE);
+            }
+
+            // Find file size after write
+            fseek(fileptr, 0, SEEK_END);
+            int filesize = ftell(fileptr);
+            fseek(fileptr, 0, SEEK_SET);
+
+            ssize_t read;
+            
+            // Begin sending data to incoming connection in BUFF_SIZE-sized chunks
+            while ((read = fread(buffer, sizeof(char), BUFF_SIZE - 1, fileptr)) > 0) {
+                // Null terminate the end of the buffer
+                buffer[read] = '\0';
+
+                ssize_t sent;
+
+                if ((sent = send(socket_fd, buffer, read, 0)) < 1) {
+                    syslog(LOG_ERR, "Error: unable to send data to incoming connection");
+                    closelog();
+                    exit(EXIT_FAILURE);
+                }
+            }
+            
+            fclose(fileptr);
+            pthread_mutex_unlock(&file_access);
+
+            // Reset for potential new incoming data
+            size = 0;
+        }
+    }
+
+    // Cleanup for thread and connection handler
+    free(buffer);
+
+    if (close(thread->socket_fd) != 0) {
+        syslog(LOG_ERR, "Error: unable to close thread socket connection");
+    }
+
+    // Mark thread as completed in params
+    thread->completed = 1;
+    
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
     // Open syslog to begin logging
     openlog("aesdsocket", LOG_CONS | LOG_PID, LOG_USER);
 
-    FILE *fileptr;
+    // Bind interrupt and terminate signals to interrupt function
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        syslog(LOG_ERR, "Error: unable to bind signal to signal handler");
+    }
+
+    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+        syslog(LOG_ERR, "Error: unable to bind signal to signal handler");
+    }
+
     struct addrinfo hints;
 
     // Set hints properties
@@ -49,11 +224,13 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    int new_socket_fd;
+
     // Check if address info can be retreived
     if (getaddrinfo(NULL, PORT, &hints, &servinfo) != 0) {
         syslog(LOG_ERR, "Error: unable to get local address info");
         closelog();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     // Get local address and prepare incoming connecting address
@@ -63,18 +240,16 @@ int main(int argc, char *argv[]) {
 
     // Check if new socket was created successfully
     if ((socket_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-        freeaddrinfo(servinfo);
         syslog(LOG_ERR, "Error: unable to create socket for listening for incoming connections");
         closelog();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     // Check if binding socket and local address was successful
     if (bind(socket_fd, &local_address, sizeof local_address) < 0) {
-        freeaddrinfo(servinfo);
         syslog(LOG_ERR, "Error: unable to bind socket and local address");
         closelog();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     // Run process as daemon if -d argument is used
@@ -90,30 +265,28 @@ int main(int argc, char *argv[]) {
 
     // Check if socket can successfully listen to incoming connections
     if (listen(socket_fd, 1) < 0) {
-        freeaddrinfo(servinfo);
         syslog(LOG_ERR, "Error: socket unable to listen for incoming connections");
         closelog();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    // Dynamically allocate memory to buffers
-    receive_buffer = malloc(BUFF_SIZE);
-    write_buffer = malloc(BUFF_SIZE);
+    // Initialize linked list
+    LIST_INIT(&list_head);
 
-    // Bind interrupt and terminate signals to interrupt function
-    signal(SIGINT, interrupt_signal_handler);
-    signal(SIGTERM, interrupt_signal_handler);
+    // Create timestamp handler thread
+    if (pthread_create(&timestamp_thread_id, NULL, timestamp_handler, NULL) != 0) {
+        syslog(LOG_ERR, "Error: unable to create thread for handling timestamps");
+        closelog();
+        exit(EXIT_FAILURE);
+    }
 
     // Main process loop
-    while (1) {
+    while (run) {
         // Check if incoming connection can be accepted and if new socket was created successfully
         if ((new_socket_fd = accept(socket_fd, &connecting_address, &connecting_address_len)) < 0) {
-            free(receive_buffer);
-            free(write_buffer);
-            freeaddrinfo(servinfo);
             syslog(LOG_ERR, "Error: unable to accept incoming connection");
             closelog();
-            return -1;
+            exit(EXIT_FAILURE);
         }
 
         // Get connecting address' IP address as string
@@ -121,99 +294,84 @@ int main(int argc, char *argv[]) {
 
         syslog(LOG_INFO, "Accepted connection from %s", connecting_address_str);
 
-        ssize_t received;
-        int size = 0;
+        // Set up and dynamically allocate memory for thread params
+        thread_data_t *thread = (thread_data_t *)malloc(sizeof(thread_data_t));
 
-        // Begin receiving data from incoming connection
-        while ((received = recv(new_socket_fd, receive_buffer, BUFF_SIZE - 1, 0)) > 0) {
-            // Null terminate the end of the receiving buffer
-            receive_buffer[received] = '\0';
-
-            // Dynamically allocate more memory to buffers if received data exceeds default buffer size
-            if (size + received >= BUFF_SIZE) {
-                ssize_t new_buff_size = size + received + 1;
-
-                char *new_receive_buffer = realloc(receive_buffer, new_buff_size);
-
-                if (new_receive_buffer == NULL) {
-                    freeaddrinfo(servinfo);
-                    syslog(LOG_ERR, "Error: unable to reallocate memory for receiving buffer");
-                    closelog();
-                    return -1;
-                }
-
-                char *new_write_buffer = realloc(write_buffer, new_buff_size);
-
-                if (new_write_buffer == NULL) {
-                    freeaddrinfo(servinfo);
-                    syslog(LOG_ERR, "Error: unable to reallocate memory for writing buffer");
-                    closelog();
-                    return -1;
-                }
-
-                receive_buffer = new_receive_buffer;
-                write_buffer = new_write_buffer;
-            }
-
-            // Copy data from receiving buffer to writing buffer and null terminate the end of the writing buffer
-            memcpy(write_buffer + size, receive_buffer, received);
-            size += received;
-            write_buffer[size] = '\0';
-
-            // Check for newline in writing buffer
-            char *newline = memchr(write_buffer, '\n', size);
-            
-            // If newline is found, open file and begin writing to file from writing buffer
-            if (newline != NULL) {
-                fileptr = fopen(filepath, "a+");
-
-                // Write to file from writing buffer
-                if (fprintf(fileptr, "%s", write_buffer) < 0) {
-                    free(receive_buffer);
-                    free(write_buffer);
-                    freeaddrinfo(servinfo);
-                    syslog(LOG_ERR, "Error: unable to write to file");
-                    closelog();
-                    return -1;
-                }
-
-                // Find file size after write
-                fseek(fileptr, 0, SEEK_END);
-                int filesize = ftell(fileptr);
-                fseek(fileptr, 0, SEEK_SET);
-
-                char *send_buffer = malloc(BUFF_SIZE);
-                ssize_t read;
-                
-                // Begin sending data to incoming connection in BUFF_SIZE-sized chunks
-                while ((read = fread(send_buffer, sizeof(char), BUFF_SIZE - 1, fileptr)) > 0) {
-                    // Null terminate the end of the sending buffer
-                    send_buffer[read] = '\0';
-
-                    ssize_t sent;
-
-                    if ((sent = send(new_socket_fd, send_buffer, read, 0)) < 1) {
-                        free(receive_buffer);
-                        free(write_buffer);
-                        free(send_buffer);
-                        freeaddrinfo(servinfo);
-                        syslog(LOG_ERR, "Error: unable to send data to incoming connection");
-                        closelog();
-                        return -1;
-                    }
-                }
-                
-                fclose(fileptr);
-
-                // Reset for potential new incoming data
-                size = 0;
-
-                free(send_buffer);
-            }
+        if (thread == NULL) {
+            syslog(LOG_ERR, "Error: unable to allocate memory for thread handling connection");
+            closelog();
+            exit(EXIT_FAILURE);
         }
 
-        syslog(LOG_INFO, "Closed connection from %s", connecting_address_str);
+        thread->socket_fd = new_socket_fd;
+        thread->completed = 0;
+
+        // Insert new thread node at head of linked list
+        pthread_mutex_lock(&thread_access);
+        LIST_INSERT_HEAD(&list_head, thread, entries);
+        pthread_mutex_unlock(&thread_access);
+
+        // Create connection handler thread
+        if (pthread_create(&thread->thread_id, NULL, connection_handler, (void *)thread) != 0) {
+            syslog(LOG_ERR, "Error: unable to create thread for handling connection");
+            closelog();
+            exit(EXIT_FAILURE);
+        }
+
+        // Join completed threads and remove them from the linked list
+        pthread_mutex_lock(&thread_access);
+        thread_data_t *iter = LIST_FIRST(&list_head);
+
+        while (iter != NULL) {
+            thread_data_t *next = LIST_NEXT(iter, entries);
+
+            if (iter->completed == 1) {
+                pthread_mutex_unlock(&thread_access);
+                pthread_join(iter->thread_id, NULL);
+                LIST_REMOVE(iter, entries);
+                pthread_mutex_lock(&thread_access);
+                free(iter);
+            }
+
+            iter = next;
+        }
+        pthread_mutex_unlock(&thread_access);
+
+        // syslog(LOG_INFO, "Closed connection from %s", connecting_address_str);
     }
 
-    return 0;
+    // Cleanup
+    pthread_cancel(timestamp_thread_id);
+    pthread_join(timestamp_thread_id, NULL);
+
+    pthread_mutex_lock(&thread_access);
+    thread_data_t *thread = LIST_FIRST(&list_head);
+
+    while (thread != NULL) {
+        thread_data_t *next = LIST_NEXT(thread, entries);
+        pthread_cancel(thread->thread_id);
+        pthread_mutex_unlock(&thread_access);
+        pthread_join(thread->thread_id, NULL);
+        LIST_REMOVE(thread, entries);
+        pthread_mutex_lock(&thread_access);
+        free(thread);
+        thread = next;
+    }
+    pthread_mutex_unlock(&thread_access);
+
+    pthread_mutex_destroy(&file_access);
+    pthread_mutex_destroy(&thread_access);
+
+    freeaddrinfo(servinfo);
+
+    if (close(socket_fd) != 0) {
+        syslog(LOG_ERR, "Error: unable to close socket connection");
+    }
+
+    if (remove(filepath) != 0) {
+        syslog(LOG_ERR, "Error: unable to delete temporary file");
+    }
+
+    closelog();
+    exit(EXIT_SUCCESS);
 }
